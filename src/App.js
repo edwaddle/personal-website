3a2f474701f1bf7093b4e1fbdd66b2a757c3f4aa
edwin yue <53625790+edwaddle@users.noreply.github.com>
2024-11-19 00:06:49 -0800

    Tried to add a nav bar, and fixed the spacing of the background images
    Need to redo the navbar, bc it kinda sucks and I don't know hwo to to implement it

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="NavBar">
+        <div className="Hole"></div>
+      </div>
+      
       <section className="Home"></section>
-      <section className="AboutMe"></section>
-      {/*
-      <section className="Projects"></section>
-      <section className="Clubs"></section>
+      <section className="AboutMe">
+        isdfubkajdfb
+      </section>
+      <section className="Projects">I' projects
+      </section>
+      <section className="Clubs">I'm clubs</section>
       <footer>this is a footer</footer>
-       */}
       
     </div>
   );
