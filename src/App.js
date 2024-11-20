1398ebec8728dede3c1824d3ed02fcc9f1e595e9
edwin yue <53625790+edwaddle@users.noreply.github.com>
2024-11-20 01:22:00 -0800

    made scroll into view, and added home screen
    Changed everything from position absolute to position relative, and I'm not sure how to style the navbar. Will continue working on the rest of the sections another time

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import {useRef} from 'react';
+
 
 function App() {
+  const homeRef = useRef();
+  const aboutMeRef = useRef();
+  const projectsRef = useRef();
+  const clubRef = useRef();
+  
   return (
     <div className="App">
+      {/*
       <div className="NavBar">
-        <div className="Hole"></div>
+        <ul className="Hole">
+        <li>
+            <a href="#projects" onClick={() => {
+              homeRef.current.scrollIntoView({ behavior: 'smooth' });
+            }}>Home</a>
+          </li>
+          <li>
+            <a href="#projects" onClick={() => {
+              aboutMeRef.current.scrollIntoView({ behavior: 'smooth' });
+            }}>About Me</a>
+          </li>
+          <li>
+            <a href="#projects" onClick={() => {
+              projectsRef.current.scrollIntoView({ behavior: 'smooth' });
+            }}>Projects</a>
+          </li>
+          <li>
+            <a href="#projects" onClick={() => {
+              clubRef.current.scrollIntoView({ behavior: 'smooth' });
+            }}>Clubs</a>
+          </li>
+        </ul>
       </div>
-      
-      <section className="Home"></section>
+      */}
+      <section className="Home">
+        <div ref={homeRef} className="innerSection">
+          <div className="introBox">
+            <h1>Hey!<br/> I'm Edwin Yue</h1>
+            <div className="getStartedBox">
+              <button onClick={() => {
+              aboutMeRef.current.scrollIntoView({ behavior: 'smooth' });
+            }} className="getStarted">Get Started!</button>
+              <img src={require('./background-images/penguin.png')} />
+            </div>
+          </div>
+        </div>
+      </section>
       <section className="AboutMe">
-        isdfubkajdfb
+        <div ref={aboutMeRef} className="innerSection">I' me</div>
       </section>
-      <section className="Projects">I' projects
+      <section  className="Projects">
+        <div ref={projectsRef} className="innerSection">I' projects</div>
       </section>
-      <section className="Clubs">I'm clubs</section>
-      <footer>this is a footer</footer>
+      <section className="Clubs">
+        <div ref={clubRef} innerSection>I' CLubs</div>
+      </section>
+      <footer></footer>
       
     </div>
   );
